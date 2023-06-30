5a281a1ed5b1372daa686a7ef20980cb935cd7d1
Rade Pejanović <radepejanovic@gmail.com>
2023-06-30 21:28:22 +0200

    feat: Created UserRepository

---
+++ Library/Library/Core/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Library.Core.Model;
+using System.Collections.Generic;
+
+namespace Library.Core.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        User Get(int id);
+        Dictionary<int, User> GetAll();
+        void Remove(int id);
+    }
+}
---
+++ Library/Library/Core/Repository/UserRepository.cs
@@ -0,0 +1,41 @@
+﻿using Library.Core.Model;
+using Library.Core.Repository.Interface;
+using Library.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ICRUDRepository<User> _repo;
+
+        public UserRepository(ICRUDRepository<User> repo)
+        {
+            _repo = repo;
+        }
+
+        public void Add(User user)
+        {
+            _repo.Add(user);
+        }
+
+        public void Remove(int id)
+        {
+            _repo.Remove(id);
+        }
+
+        public User Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public Dictionary<int, User> GetAll()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
