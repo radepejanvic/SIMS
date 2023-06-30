44550d3dd6053760efcbb7975e69ab326b89c3eb
Rade Pejanović <radepejanovic@gmail.com>
2023-06-30 18:47:15 +0200

    feat: Created new project

---
+++ Library/Core/Serializers/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Serializers
+{
+    public interface ISerializer<T> where T : ISerializable, new()
+    {
+        Dictionary<int, T> Load();
+        void Save(Dictionary<int, T> objects);
+    }
+}
