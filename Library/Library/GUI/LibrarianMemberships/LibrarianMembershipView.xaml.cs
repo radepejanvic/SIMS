49adbd5a1827caa3d3a89b9d49077bde1aca185c
Rade Pejanović <radepejanovic@gmail.com>
2023-07-01 01:14:54 +0200

    feat: Creeated VM and V base for LibrarianMemberships

---
+++ Library/Library/GUI/LibrarianMemberships/LibrarianMembershipView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Library.GUI.LibrarianMemberships;
+
+public partial class LibrarianMembershipView : UserControl
+{
+    public LibrarianMembershipView()
+    {
+        InitializeComponent();
+    }
+}
