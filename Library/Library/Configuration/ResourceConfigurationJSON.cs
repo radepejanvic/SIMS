a9f125206775733a37540b5036232f76f7d8a04d
Rade Pejanović <radepejanovic@gmail.com>
2023-06-30 23:28:22 +0200

    feat: Added configuration for login and some basic operations

--- Library/Library/Configuration/ResourceConfigurationJSON.cs
+++ Library/Library/Configuration/ResourceConfigurationJSON.cs
@@ -3,76 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Library.Core.Model;
 using Library.Model;
-using Library.Model.Checkup;
-using Library.Model.Refferal;
 
 namespace Library.Configuration
 {
     public class ResourceConfigurationJSON<T> : IResourceConfiguration<T>
     {
-        private string PatientJSON = @"..\..\..\Data\patient.json";
-        private string DoctorJSON = @"..\..\..\Data\doctor.json";
-        private string DoctorScheduleJSON = @"..\..\..\Data\doctorSchedule.json";
-        private string DirectorJSON = @"..\..\..\Data\director.json";
-        private string NurseJSON = @"..\..\..\Data\nurse.json";
-        private string DynamicalEquipmentRequestJSON = @"..\..\..\Data\dynamicalEquipmentRequest.json";
-        private string AppointmentNotificationJSON = @"..\..\..\Data\appointmentNotification.json";
-        private string RoomJSON = @"..\..\..\Data\room.json";
-        private string AppointmentJSON = @"..\..\..\Data\appointment.json";
-        private string AnamnesisJSON = @"..\..\..\Data\anamnesis.json";
-        private string DoctorRefferalJSON = @"..\..\..\Data\doctorRefferal.json";
-        private string HospitalRefferalJSON = @"..\..\..\Data\hospitalRefferal.json";
-        private string PerscriptionJSON = @"..\..\..\Data\perscription.json";
-        private string DrugJSON = @"..\..\..\Data\drug.json";
-        private string RoomScheduleJSON = @"..\..\..\Data\roomSchedule.json";
-        private string DrugOrderJSON = @"..\..\..\Data\drugOrder.json";
-        private string RoomRenovationJSON = @"..\..\..\Data\roomRenovation.json";
-        private string RoomDefusingRenovationJSON = @"..\..\..\Data\roomDefusingRenovation.json";
-        private string RoomFusingRenovationJSON = @"..\..\..\Data\roomFusingRenovation.json";
-        private string LeaveRequestJSON = @"..\..\..\Data\leaveRequest.json";
-        private string PatientPlacementJSON = @"..\..\..\Data\patientPlacement.json";
-        private string PatientCheckupJSON = @"..\..\..\Data\patientCheckup.json";
+        private string UserJSON = @"..\..\..\Data\user.json";
+        private string PersonJSON = @"..\..\..\Data\person.json";
+        private string MembershipJSON = @"..\..\..\Data\membership.json";
+        private string MembershipCardJSON = @"..\..\..\Data\membershipCard.json";
+        private string BookTitleJSON = @"..\..\..\Data\bookTitle.json";
+        private string BookCopyJSON = @"..\..\..\Data\bookCopy.json";
+        private string PublisherJSON = @"..\..\..\Data\publisher.json";
+        private string AuthorJSON = @"..\..\..\Data\author.json";
+        private string LibraryBranchJSON = @"..\..\..\Data\libraryBranch.json";
+        private string LoanJSON = @"..\..\..\Data\loan.json";
 
-        private readonly string CustomNotificationJSON = @"..\..\..\Data\customNotification.json";
-        private readonly string CustomNotificationConfigurationJSON = @"..\..\..\Data\customNotificationConfiguration.json";
-        private readonly string HospitalSurveyJSON = @"..\..\..\Data\hospitalSurvey.json";
-        private readonly string DoctorSurveyJSON = @"..\..\..\Data\doctorSurvey.json";
-        private readonly string MessagesJSON = @"..\..\..\Data\messages.json";
 
         public string GetResourcePath()
         {
             return typeof(T).Name switch
             {
-                nameof(Patient) => PatientJSON,
-                nameof(Doctor) => DoctorJSON,
-                nameof(DoctorSchedule) => DoctorScheduleJSON,
-                nameof(RoomSchedule) => RoomScheduleJSON,
-                nameof(Director) => DirectorJSON,
-                nameof(Nurse) => NurseJSON,
-                nameof(DynamicalEquipmentRequest) => DynamicalEquipmentRequestJSON,
-                nameof(AppointmentNotification) => AppointmentNotificationJSON,
-                nameof(Room) => RoomJSON,
-                nameof(Appointment) => AppointmentJSON,
-                nameof(Anamnesis) => AnamnesisJSON,
-                nameof(DoctorRefferal) => DoctorRefferalJSON, 
-                nameof(HospitalRefferal) => HospitalRefferalJSON, 
-                nameof(Perscription) => PerscriptionJSON,
-                nameof(Drug) => DrugJSON,
-                nameof(DrugOrder) => DrugOrderJSON,
-                nameof(CustomNotification) => CustomNotificationJSON,
-                nameof(CustomNotificationConfiguration) => CustomNotificationConfigurationJSON,
-                nameof(RoomRenovation) => RoomRenovationJSON,
-                nameof(RoomDefusingRenovation) => RoomDefusingRenovationJSON,
-                nameof(RoomFusingRenovation) => RoomFusingRenovationJSON,
-                nameof(LeaveRequest) => LeaveRequestJSON,
-                nameof(HospitalSurvey) => HospitalSurveyJSON,
-                nameof(DoctorSurvey) => DoctorSurveyJSON,
-                nameof(PatientPlacement) => PatientPlacementJSON,
-                nameof(PatientCheckup) => PatientCheckupJSON,
-                nameof(Message) => MessagesJSON,
-                _ => string.Empty,
+                nameof(User) => UserJSON,
+                nameof(Person) => PersonJSON,
+                nameof(Membership) => MembershipJSON,
+                nameof(MembershipCard) => MembershipCardJSON,
+                nameof(BookTitle) => BookTitleJSON,
+                nameof(BookCopy) => BookCopyJSON,
+                nameof(Publisher) => PublisherJSON,
+                nameof(Author) => AuthorJSON,
+                nameof(LibraryBranch) => LibraryBranchJSON,
+                nameof(Loan) => LoanJSON,
+                _ => string.Empty
             };
+
         }
     }
 }
