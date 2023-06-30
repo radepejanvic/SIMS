3c23871e2fa4aa4dac8282ebbd8003d9b3d31204
Rade Pejanović <radepejanovic@gmail.com>
2023-06-30 18:00:25 +0200

    feat: Created enums

---
+++ Library/Library/Enum/PaymentReason.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Helpers.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Enum
+{
+    public enum PaymentReason
+    {
+        MEMBERSHIP, DAMAGE, LOSS, DELAY
+    }
+}
