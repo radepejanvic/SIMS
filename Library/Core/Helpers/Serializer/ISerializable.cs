a9b93595c5c50854a946be29bc6408e085dfb821
Rade Pejanović <radepejanovic@gmail.com>
2023-06-30 18:04:04 +0200

    feat: Moved the project to SIMS directory

---
+++ Library/Core/Helpers/Serializer/ISerializable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Helpers.Serializer
+{
+    public interface ISerializable
+    {
+        public int Id { get; set; }
+    }
+}
