6eea863e394058141e9c781f061a41cd6d0bc059
Rade Pejanović <radepejanovic@gmail.com>
2023-07-01 01:14:22 +0200

    feat: Creeated VM and V base for LibrarianCollection

--- Library/Library/GUI/LibrarianCollection/LibrarianCollectionViewModel.cs
+++ Library/Library/GUI/LibrarianCollection/LibrarianCollectionViewModel.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Library.GUI.LibrarianCollection
 {
     public class LibrarianCollectionViewModel : ViewModelBase
     {
         private readonly User _user;
+
+        public ICommand OpenBookLoaning { get; }
+        public ICommand OpenBookRetrieval { get; }
+
         public LibrarianCollectionViewModel(User user)
         {
             _user = user;
