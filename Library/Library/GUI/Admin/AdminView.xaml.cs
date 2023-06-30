0b167de07882fbebf801865f3ce454089869c898
Rade Pejanović <radepejanovic@gmail.com>
2023-07-01 01:13:41 +0200

    feat: Creeated VM and V base for Admin

---
+++ Library/Library/GUI/Admin/AdminView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Library.GUI.Admin;
+
+public partial class AdminView : UserControl
+{
+    public AdminView()
+    {
+        InitializeComponent();
+    }
+}
