9cd912af8f3bbf0f752e5730e554327eb468ae6d
Rade Pejanović <radepejanovic@gmail.com>
2023-07-01 23:48:41 +0200

    feat: Created LoaningService

---
+++ Library/Library/Core/Service/LoaningService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Service
+{
+    public class LoaningService
+    {
+        private readonly IMembershipCardRepository _membershipCardRepo;
+        private readonly ILoanRepository _loanRepo;
+        public LoaningService(IMembershipCardRepository membershipCardRepo, ILoanRepository loanRepo) 
+        {
+            _membershipCardRepo = membershipCardRepo;
+            _loanRepo = loanRepo;
+
+        }
+    }
+}
