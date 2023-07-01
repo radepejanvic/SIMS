84577c9e39e98d2945a1e1417008fdf870ce143a
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-02 00:01:50 +0200

    feat: Created InventoryBookRepository

---
+++ Library/Library/Core/Repository/Interface/IInventoryBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Model;
+using System.Collections.Generic;
+
+namespace Library.Core.Repository.Interface
+{
+    public interface IInventoryBookRepository
+    {
+        void Add(InventoryBook inventoryBook);
+        InventoryBook Get(int id);
+        Dictionary<int, InventoryBook> GetAll();
+        void Remove(int id);
+        void Update(InventoryBook inventoryBook);
+    }
+}
---
+++ Library/Library/Core/Repository/InventoryBookRepository.cs
@@ -0,0 +1,45 @@
+﻿using Library.Core.Model;
+using Library.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Repository
+{
+    public class InventoryBookRepository : IInventoryBookRepository
+    {
+        private readonly ICRUDRepository<InventoryBook> _repo;
+
+        public InventoryBookRepository(ICRUDRepository<InventoryBook> repo)
+        {
+            _repo = repo;
+        }
+
+        public void Add(InventoryBook inventoryBook)
+        {
+            _repo.Add(inventoryBook);
+        }
+
+        public void Update(InventoryBook inventoryBook)
+        {
+            _repo.Update(inventoryBook);
+        }
+
+        public void Remove(int id)
+        {
+            _repo.Remove(id);
+        }
+
+        public InventoryBook Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public Dictionary<int, InventoryBook> GetAll()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
