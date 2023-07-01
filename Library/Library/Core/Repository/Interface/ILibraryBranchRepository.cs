cd874949080b62107a6872f2e8752754d28859a4
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-02 00:02:25 +0200

    feat: Created LibraryBranchRepository

---
+++ Library/Library/Core/Repository/Interface/ILibraryBranchRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Model;
+using System.Collections.Generic;
+
+namespace Library.Core.Repository.Interface
+{
+    public interface ILibraryBranchRepository
+    {
+        void Add(LibraryBranch libraryBranch);
+        LibraryBranch Get(int id);
+        Dictionary<int, LibraryBranch> GetAll();
+        void Remove(int id);
+        void Update(LibraryBranch libraryBranch);
+    }
+}
---
+++ Library/Library/Core/Repository/LibraryBranchRepository.cs
@@ -0,0 +1,45 @@
+﻿using Library.Core.Model;
+using Library.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Repository
+{
+    public class LibraryBranchRepository : ILibraryBranchRepository
+    {
+        private readonly ICRUDRepository<LibraryBranch> _repo;
+
+        public LibraryBranchRepository(ICRUDRepository<LibraryBranch> repo)
+        {
+            _repo = repo;
+        }
+
+        public void Add(LibraryBranch libraryBranch)
+        {
+            _repo.Add(libraryBranch);
+        }
+
+        public void Update(LibraryBranch libraryBranch)
+        {
+            _repo.Update(libraryBranch);
+        }
+
+        public void Remove(int id)
+        {
+            _repo.Remove(id);
+        }
+
+        public LibraryBranch Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public Dictionary<int, LibraryBranch> GetAll()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
