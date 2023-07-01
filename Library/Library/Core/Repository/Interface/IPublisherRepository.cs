19f6581f051203b5100024eb6d77b8ad9cdceb58
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-01 23:41:42 +0200

    feat: Created PublisherRepository

---
+++ Library/Library/Core/Repository/Interface/IPublisherRepository.cs
@@ -0,0 +1,18 @@
+﻿using Library.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Repository.Interface
+{
+    public interface IPublisherRepository
+    {
+        void Add(Publisher publisher);
+        Publisher Get(int id);
+        Dictionary<int, Publisher> GetAll();
+        void Remove(int id);
+        void Update(Publisher publisher);
+    }
+}
