53d364ae4e522f716e633f1f9572c01b6fe2bba3
Rade Pejanović <radepejanovic@gmail.com>
2023-07-01 23:54:42 +0200

    feat: Created LoanRepository

---
+++ Library/Library/Core/Repository/Interface/ILoanRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Model;
+using System.Collections.Generic;
+
+namespace Library.Core.Repository.Interface
+{
+    public interface ILoanRepository
+    {
+        void Add(Loan loan);
+        Loan Get(int id);
+        Dictionary<int, Loan> GetAll();
+        void Remove(int id);
+        void Update(Loan loan);
+    }
+}
--- Library/Library/Core/Service/LoaningService.cs
+++ Library/Library/Core/Service/LoaningService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Library.Core.Repository.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
