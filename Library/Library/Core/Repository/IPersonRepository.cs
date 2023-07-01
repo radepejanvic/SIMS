140732c6088020df75735e300aac6dc65b7213eb
Rade Pejanović <radepejanovic@gmail.com>
2023-07-01 15:46:31 +0200

    feat: Created person repository

---
+++ Library/Library/Core/Repository/IPersonRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Model;
+using System.Collections.Generic;
+
+namespace Library.Core.Repository
+{
+    public interface IPersonRepository
+    {
+        void Add(Person person);
+        Person Get(int id);
+        Dictionary<int, Person> GetAll();
+        void Remove(int id);
+        void Update(Person person);
+    }
+}
