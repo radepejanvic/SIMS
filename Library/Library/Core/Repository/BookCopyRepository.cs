f3427cb42b40cc97ac1e3e772d8e32e899cbec21
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-01 23:39:39 +0200

    feat: Created BookCopyRepository

---
+++ Library/Library/Core/Repository/BookCopyRepository.cs
@@ -0,0 +1,46 @@
+﻿using Library.Core.Model;
+using Library.Core.Repository.Interface;
+using Library.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Repository
+{
+    public class BookCopyRepository : IBookCopyRepository
+    {
+        private readonly ICRUDRepository<BookCopy> _repo;
+
+        public BookCopyRepository(ICRUDRepository<BookCopy> repo)
+        {
+            _repo = repo;
+        }
+
+        public void Add(BookCopy bookCopy)
+        {
+            _repo.Add(bookCopy);
+        }
+
+        public void Update(BookCopy bookCopy)
+        {
+            _repo.Update(bookCopy);
+        }
+
+        public void Remove(int id)
+        {
+            _repo.Remove(id);
+        }
+
+        public BookCopy Get(int id)
+        {
+            return _repo.Get(id);
+        }
+
+        public Dictionary<int, BookCopy> GetAll()
+        {
+            return _repo.GetAll();
+        }
+    }
+}
