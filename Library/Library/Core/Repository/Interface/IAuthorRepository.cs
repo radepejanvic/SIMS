879b27c6273dfbd2c4f862ae343336f825e14b08
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-01 23:37:37 +0200

    feat: Created AuthorRepository

---
+++ Library/Library/Core/Repository/Interface/IAuthorRepository.cs
@@ -0,0 +1,14 @@
+﻿using Library.Core.Model;
+using System.Collections.Generic;
+
+namespace Library.Core.Repository.Interface
+{
+    public interface IAuthorRepository
+    {
+        void Add(Author author);
+        Author Get(int id);
+        Dictionary<int, Author> GetAll();
+        void Remove(int id);
+        void Update(Author author);
+    }
+}
