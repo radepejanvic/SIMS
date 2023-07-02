8a9d7a4f19cd8c8745f1cc4d9b41fa876339fa64
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-02 14:55:24 +0200

    feat: Added functionality to OpenBookLoaningCommand

--- Library/Library/GUI/LibrarianCollection/Commands/OpenBookLoaningCommand.cs
+++ Library/Library/GUI/LibrarianCollection/Commands/OpenBookLoaningCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.Commands;
+using Library.GUI.LibrarianCollection.BookLoaning;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace Library.GUI.LibrarianCollection.Commands
 {
-    internal class OpenBookLoaningCommand
+    public class OpenBookLoaningCommand : CommandBase
     {
+        public OpenBookLoaningCommand() 
+        {
+
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var popup = new BookLoaningView();
+            popup.DataContext = new BookLoaningViewModel();
+            popup.ShowDialog();
+        }
     }
 }
