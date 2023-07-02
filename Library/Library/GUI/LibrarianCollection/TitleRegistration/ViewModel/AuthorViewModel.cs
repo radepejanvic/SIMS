ffa5d2aa5fc3248e9c8acc641fb8218ce4c304d5
Rade Pejanović <radepejanovic@gmail.com>
2023-07-02 22:31:01 +0200

    feat: Created AuthorVM

---
+++ Library/Library/GUI/LibrarianCollection/TitleRegistration/ViewModel/AuthorViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Library.Core.Model;
+using Library.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.GUI.LibrarianCollection.TitleRegistration.ViewModel
+{
+    public class AuthorViewModel : ViewModelBase
+    {
+        private readonly Author _author;
+
+        public int Id => _author.Id;
+        public string Name => _author.Name;
+        public string Surname => _author.Surname;
+
+        public AuthorViewModel(Author author)
+        {
+            _author = author;
+        }
+
+        public bool Contains(string keyword)
+        {
+            return $"{Id}--{Name}--{Surname}".ToLower().Contains(keyword.ToLower());
+        }
+    }
+}
