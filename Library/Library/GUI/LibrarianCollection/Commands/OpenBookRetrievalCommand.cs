820805cdcd1361d1b9ee7ca91b3dd7cc676f9888
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-02 14:55:58 +0200

    feat: Added functionality to OpenBookRetrievalCommand

--- Library/Library/GUI/LibrarianCollection/Commands/OpenBookRetrievalCommand.cs
+++ Library/Library/GUI/LibrarianCollection/Commands/OpenBookRetrievalCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.Commands;
+using Library.GUI.LibrarianCollection.BookRetrieval;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace Library.GUI.LibrarianCollection.Commands
 {
-    internal class OpenBookRetrievalCommand
+    public class OpenBookRetrievalCommand : CommandBase
     {
+        public OpenBookRetrievalCommand() 
+        {
+
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var popup = new BookRetrievalView();
+            popup.DataContext = new BookRetrievalViewModel();
+            popup.ShowDialog();
+        }
     }
 }
