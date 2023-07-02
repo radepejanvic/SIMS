50dd3e312fd824410460b66a191b928f2fed4fbf
Rade Pejanović <radepejanovic@gmail.com>
2023-07-02 22:31:32 +0200

    feat: Created PublisherVm

---
+++ Library/Library/GUI/LibrarianCollection/TitleRegistration/ViewModel/PublisherViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Library.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.GUI.LibrarianCollection.TitleRegistration.ViewModel
+{
+    public class PublisherViewModel
+    {
+        private readonly Publisher _publisher;
+        public int Id => _publisher.Id;
+        public string Name => _publisher.Name;
+        public Address Address => _publisher.Address;
+
+        public PublisherViewModel(Publisher publisher)
+        {
+            _publisher = publisher;
+        }
+
+        public bool Contains(string keyword)
+        {
+            return $"{Id}--{Name}--{Address}".ToLower().Contains(keyword.ToLower());
+        }
+    }
+}
