4899ab7de8eb1d06672ac0f9733cc8d875a15b51
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-02 14:57:00 +0200

    feat: Added functionality to OpenTitleRegistrationCommand

--- Library/Library/GUI/LibrarianCollection/Commands/OpenTitleRegistrationCommand.cs
+++ Library/Library/GUI/LibrarianCollection/Commands/OpenTitleRegistrationCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.Commands;
+using Library.GUI.LibrarianCollection.TitleRegistration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace Library.GUI.LibrarianCollection.Commands
 {
-    internal class OpenTitleRegistrationCommand
+    public class OpenTitleRegistrationCommand : CommandBase
     {
+        public OpenTitleRegistrationCommand() 
+        {
+
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var popup = new TitleRegistrationView();
+            popup.DataContext = new TitleRegistrationViewModel();
+            popup.ShowDialog();
+        }
     }
 }
