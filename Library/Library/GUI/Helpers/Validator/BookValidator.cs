07c53ec9fd3b9a184cbc580ed6b9aea894136d12
Rade Pejanović <radepejanovic@gmail.com>
2023-07-02 23:48:11 +0200

    feat: Created BookValidator

---
+++ Library/Library/GUI/Helpers/Validator/BookValidator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.GUI.Helpers.Validation
+{
+    public static class BookValidator
+    {
+        public static bool CheckPublicationYear(string year)
+        {
+            return DateTime.TryParseExact(year, "yyyy.", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+        }
+
+        public static bool CheckISBN(string ISBN)
+        {
+            return ISBN?.Length == 13;
+        }
+
+        public static bool CheckUDK(string UDK)
+        {
+            return UDK?.Length == 10;
+        }
+    }
+}
