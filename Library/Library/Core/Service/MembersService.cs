2e9507fa5c033b3a50c0a0f26276f442ea134707
Rade Pejanović <radepejanovic@gmail.com>
2023-07-02 13:00:34 +0200

    feat: Created members service

---
+++ Library/Library/Core/Service/MembersService.cs
@@ -0,0 +1,29 @@
+﻿using Library.Core.Model;
+using Library.Core.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Core.Service
+{
+    public class MembersService
+    {
+        public readonly IMembershipCardRepository _membershipCardRepo;
+        public readonly IMembershipCardRepository _membershipRepo;
+        public readonly IPersonRepository _personRepo;
+
+        public MembersService(IMembershipCardRepository membershipCardRepo, IMembershipCardRepository membershipRepo, IPersonRepository personRepo)
+        {
+            _membershipCardRepo = membershipCardRepo;
+            _membershipRepo = membershipRepo;
+            _personRepo = personRepo;
+        }
+
+        //public Dictionary<int, PersonMembershipCardDTO> GetAllPersonMembershipCardDTOs()
+        //{
+            
+        //}
+    }
+}
