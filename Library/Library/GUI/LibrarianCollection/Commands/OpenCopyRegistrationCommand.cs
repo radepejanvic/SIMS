ba996fa909b1cda3a02a41b555e4042205e7c5f0
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-02 14:56:27 +0200

    feat: Added functionality to OpenCopyRegistrationCommand

--- Library/Library/GUI/LibrarianCollection/Commands/OpenCopyRegistrationCommand.cs
+++ Library/Library/GUI/LibrarianCollection/Commands/OpenCopyRegistrationCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Library.Commands;
+using Library.GUI.LibrarianCollection.CopyRegistration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,23 @@ using System.Threading.Tasks;
 
 namespace Library.GUI.LibrarianCollection.Commands
 {
-    internal class OpenCopyRegistrationCommand
+    public class OpenCopyRegistrationCommand : CommandBase
     {
+        public OpenCopyRegistrationCommand() 
+        {
+
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            var popup = new CopyRegistrationView();
+            popup.DataContext = new CopyRegistrationViewModel();
+            popup.ShowDialog();
+        }
     }
 }
