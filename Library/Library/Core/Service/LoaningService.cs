aade7781e71de0df04c213866a33b59aba030f6b
Rade Pejanović <radepejanovic@gmail.com>
2023-07-02 13:00:20 +0200

    feat: Added new attributes

--- Library/Library/Core/Service/LoaningService.cs
+++ Library/Library/Core/Service/LoaningService.cs
@@ -1,4 +1,5 @@
-﻿using Library.Core.Repository.Interface;
+﻿using Library.Core.Model;
+using Library.Core.Repository.Interface;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,28 +11,42 @@ namespace Library.Core.Service
     public class LoaningService
     {
         private readonly IMembershipCardRepository _membershipCardRepo;
+        private readonly IMembershipRepository _membershipRepo;
+        private readonly IBookCopyRepository _bookCopyRepo;
         private readonly ILoanRepository _loanRepo;
 
-        public LoaningService(IMembershipCardRepository membershipCardRepo, ILoanRepository loanRepo) 
+
+        public LoaningService(IMembershipCardRepository membershipCardRepo, ILoanRepository loanRepo, IBookCopyRepository bookCopyRepo, IMembershipRepository membershipRepo) 
         {
             _membershipCardRepo = membershipCardRepo;
             _loanRepo = loanRepo;
+            _bookCopyRepo = bookCopyRepo;
+            _membershipRepo = membershipRepo;
+        }
+
+        public void LoanBook(int membershipCardId, int bookCopyId)
+        {
+            var membershipCard = _membershipCardRepo.Get(membershipCardId);
+            var membership = _membershipRepo.Get(membershipCard.MembershipId);
+            var bookCopy = _bookCopyRepo.Get(bookCopyId);
+            _loanRepo.Add(new Loan(bookCopy.InventoryNumber, membershipCardId, DateTime.Today.AddDays(membership.LoanDuration), null));
+        }
+
+        public bool HasReachedTheLimit(int membershipCardId)
+        {
+            var membershipCard = _membershipCardRepo.Get(membershipCardId);
+            var membership = _membershipRepo.Get(membershipCard.MembershipId);
+            return _loanRepo.GetNumberOfActiveLoans(membershipCardId) == membership.LoanCount;
+        }
+
+        public void RetrieveBook()
+        {
 
         }
 
-        public void LoanBook(int membershipId, int bookCopyId)
+        public Dictionary<int, BookCopy> GetAllAvaliableBooks()
         {
-            
-        }
-
-        public void IsAvaliable(int bookCopyId)
-        {
-
-        }
-
-        public void ReturnBook()
-        {
-
+            return _bookCopyRepo.GetAllAvaliableBooks(_loanRepo.GetAllLoanedBooks());
         }
     }
 }
