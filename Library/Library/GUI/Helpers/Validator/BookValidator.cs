5a5de01e12cb686d191e9e6bd3fbedc91902e34b
Rade Pejanović <radepejanovic@gmail.com>
2023-07-03 11:21:07 +0200

    feat: Added CheckAuthors function

--- Library/Library/GUI/Helpers/Validator/BookValidator.cs
+++ Library/Library/GUI/Helpers/Validator/BookValidator.cs
@@ -12,7 +12,7 @@ namespace Library.GUI.Helpers.Validation
     {
         public static bool CheckPublicationYear(string year)
         {
-            return DateTime.TryParseExact(year, "yyyy.", CultureInfo.InvariantCulture, DateTimeStyles.None, out _);
+            return DateTime.TryParseExact(year, "yyyy.", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime date) && date <= DateTime.Today;
         }
 
         public static bool CheckISBN(string ISBN)
@@ -23,6 +23,11 @@ namespace Library.GUI.Helpers.Validation
         public static bool CheckUDK(string UDK)
         {
             return UDK?.Length == 10;
+        }
+
+        public static bool CheckAuthors(List<int> authors)
+        {
+            return authors.Count != 0;
         }
     }
 }
