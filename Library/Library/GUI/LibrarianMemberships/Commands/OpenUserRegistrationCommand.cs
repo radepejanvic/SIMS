72b120566d7b3208b8f6759ecafb842f1cc32f73
AlexanderMishutkin <alex.type59@gmail.com>
2023-07-03 23:25:45 +0200

    Copied menu for Member Librarian

---
+++ Library/Library/GUI/LibrarianMemberships/Commands/OpenUserRegistrationCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using Library.Commands;
+using Library.Core.Service.Interface;
+using Library.GUI.LibrarianCollection.TitleRegistration;
+using Library.GUI.LibrarianCollection.TitleRegistration.ViewModel;
+
+namespace Library.GUI.LibrarianMemberships.Commands
+{
+    public class OpenUserRegistrationCommand : CommandBase
+    {
+        private readonly IMembersService _membersService;
+        public OpenUserRegistrationCommand(IMembersService membersService) 
+        {
+            _membersService = membersService;
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Debug.WriteLine("MY WINDOW (c) Alex M.");
+        }
+    }
+}
