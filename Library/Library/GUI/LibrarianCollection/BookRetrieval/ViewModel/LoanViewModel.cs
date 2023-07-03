7a7042a4fd1f3a3dc9ff4ad922ad793c94cb5df2
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-03 11:45:07 +0200

    feat: Created LoanViewModel

--- Library/Library/GUI/LibrarianCollection/BookRetrieval/ViewModel/LoanViewModel.cs
+++ Library/Library/GUI/LibrarianCollection/BookRetrieval/ViewModel/LoanViewModel.cs
@@ -1,6 +1,24 @@
+using System;
+using Library.Core.Model;
+using Library.ViewModel;
+
 namespace Library.GUI.LibrarianCollection.BookRetrieval.ViewModel;
 
-public class LoanViewModel
+public class LoanViewModel : ViewModelBase
 {
-    
+    private readonly Loan _loan;
+    public int Id => _loan.Id;
+    public string InventoryNumber => _loan.InventoryNumber;
+    public int MembershipCardId => _loan.MembershipCardId;
+    public DateTime ExpirationDate => _loan.ExpirationDate;
+
+    public LoanViewModel()
+    {
+        
+    }
+
+    public LoanViewModel(Loan loan)
+    {
+        _loan = loan;
+    }
 }
