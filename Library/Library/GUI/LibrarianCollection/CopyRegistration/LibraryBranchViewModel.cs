27e0fe1df775ce83439467eb98406e94a5de27c7
Rade Pejanović <radepejanovic@gmail.com>
2023-07-03 12:17:41 +0200

    feat: Created LibraryBranchViewModel

---
+++ Library/Library/GUI/LibrarianCollection/CopyRegistration/LibraryBranchViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Library.Core.Model;
+using Library.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.GUI.LibrarianCollection.CopyRegistration
+{
+    public class LibraryBranchViewModel : ViewModelBase
+    {
+        private readonly LibraryBranch _branch;
+
+        public int Id => _branch.Id;
+        public string Name => _branch.Name;
+
+
+        public LibraryBranchViewModel(LibraryBranch branch)
+        {
+            _branch = branch;
+        }
+    }
+}
