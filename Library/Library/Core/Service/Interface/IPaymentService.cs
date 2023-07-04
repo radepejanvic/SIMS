03209ae7089f91dd1baefdd26d6c76ff1ddf5715
pejinovics <75089955+pejinovics@users.noreply.github.com>
2023-07-04 22:16:23 +0200

    feat: Added PaymentService

---
+++ Library/Library/Core/Service/Interface/IPaymentService.cs
@@ -0,0 +1,12 @@
+﻿using Library.Core.Model;
+using System;
+
+namespace Library.Core.Service.Interface
+{
+    public interface IPaymentService
+    {
+        float GetPrice(int MembershipCardId, DateTime date);
+        float GetPrice(string inventoryNumber);
+        void Add(Payment payment);
+    }
+}
